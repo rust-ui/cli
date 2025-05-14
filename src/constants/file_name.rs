a0d36b21c087876f96bf975500589a752cc41237
Mayank <cmayank949@gmail.com>
2025-05-14 23:27:24 +0530

    app_config.toml creation and update of dependencies

--- src/constants/file_name.rs
+++ src/constants/file_name.rs
@@ -2,6 +2,7 @@
 pub struct FILE_NAME;
 
 impl FILE_NAME {
+    pub const APP_CONFIG_TOML: &str = "app_config.toml";
     pub const TAILWIND_CONFIG_JS: &str = "tailwind.config.js";
     pub const COMPONENTS_TOML: &str = "Components.toml";
     pub const PACKAGE_JSON: &str = "package.json";
