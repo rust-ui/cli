940ba04c92551ec24a00aa81ffec9b07b7c1f3b0
Max Wells <maxwells.pro@proton.me>
2025-05-17 12:10:28 +0400

    (refacto): `init`: Remove unused functions.

--- src/command_init/config.rs
+++ src/command_init/config.rs
@@ -1,15 +1,12 @@
-// use dotenv::dotenv;
 use indicatif::ProgressBar;
-// use serde::de::Error;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
-// use std::fmt::Result;
-// use std::env;
-use crate::constants::dependencies::INIT_DEPENDENCIES;
-use crate::constants::others::{CARGO_TOML_FILE, SPINNER_UPDATE_DURATION};
 use std::fs;
 use std::process::Command;
 use std::time::Duration;
+
+use crate::constants::dependencies::INIT_DEPENDENCIES;
+use crate::constants::others::SPINNER_UPDATE_DURATION;
 
 ///
 /// UiConfig
@@ -65,7 +62,7 @@ impl Default for UiConfig {
 /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
 /*                     ✨ FUNCTIONS ✨                        */
 /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-#[allow(unused)]
+
 pub async fn add_init_dependencies() {
     for dep in INIT_DEPENDENCIES {
         let spinner = ProgressBar::new_spinner();
@@ -89,67 +86,6 @@ pub async fn add_init_dependencies() {
                 "🔸 Error adding crates: {}",
                 String::from_utf8_lossy(&output.stderr)
             ));
-        }
-    }
-}
-
-#[allow(unused)]
-fn add_tailwind_fuse_and_leptos_use() {
-    let spinner = ProgressBar::new_spinner();
-    spinner.set_message("Adding crates: rustui_merge and leptos-use");
-    spinner.enable_steady_tick(Duration::from_millis(SPINNER_UPDATE_DURATION));
-
-    let output = Command::new("cargo")
-        .args([
-            "add",
-            "rustui_merge@0.3.0",
-            "--features",
-            "rustui_merge@0.3.0/variant,rustui_merge@0.3.0/debug",
-            "leptos-use@0.13.5",
-            "--features",
-            "leptos-use@0.13.5/storage,leptos-use@0.13.5/docs,leptos-use@0.13.5/math",
-        ])
-        .output()
-        .expect("🔸 Failed to execute cargo add command");
-
-    if output.status.success() {
-        spinner.finish_with_message("✔️ Crates added successfully.");
-    } else {
-        spinner.finish_with_message(format!(
-            "🔸 Error adding crates: {}",
-            String::from_utf8_lossy(&output.stderr)
-        ));
-    }
-}
-
-#[allow(unused)]
-fn handle_adding_leptos_use_to_ssr_features() {
-    match fs::read_to_string(CARGO_TOML_FILE) {
-        Ok(mut contents) => {
-            if let Some(start_pos) = contents.find("ssr = [") {
-                // Find the position to insert the new features
-                if let Some(end_pos) = contents[start_pos..].find(']') {
-                    let insert_pos = start_pos + end_pos;
-                    let new_features = r#"    "leptos-use/ssr",
-    "leptos-use/axum",
-"#;
-
-                    // Check if the features are already present to avoid duplicates
-                    if !contents[start_pos..insert_pos].contains("leptos-use/ssr") {
-                        contents.insert_str(insert_pos, new_features);
-                    }
-                }
-            } else {
-                println!("'ssr' feature not found.");
-            }
-
-            // Write the modified contents back to the file
-            if let Err(e) = fs::write(CARGO_TOML_FILE, &contents) {
-                eprintln!("Error writing to file: {}", e);
-            }
-        }
-        Err(e) => {
-            eprintln!("Error reading file: {}", e);
         }
     }
 }
--- src/constants/others.rs
+++ src/constants/others.rs
@@ -1,5 +1,3 @@
 pub const SPINNER_UPDATE_DURATION: u64 = 100;
 
 pub const TAILWIND_DEPENDENCIES: [&str; 3] = ["@tailwindcss/cli", "tailwindcss", "tw-animate-css"];
-
-pub const CARGO_TOML_FILE: &str = "Cargo.toml";
