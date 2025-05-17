8ccb3532d34f2dc1ccba0f5620a70e6cbd088cb0
Max Wells <maxwells.pro@proton.me>
2025-05-17 12:46:38 +0400

    (refacto): Remove `#[allow(unused)]`.

--- src/constants/dependencies.rs
+++ src/constants/dependencies.rs
@@ -1,37 +1,24 @@
-#[allow(unused)]
 pub struct Dependency<'a> {
     pub name: &'a str,
+    #[allow(unused)] // TODO. Find a way to set the version to "0.8" instead of "0.8.2".
     pub version: Option<&'a str>,
-    pub features: &'a [&'a str]
+    pub features: &'a [&'a str],
 }
 
 impl<'a> Dependency<'a> {
-    const fn new(
-        name: &'a str,
-        version: Option<&'a str>,
-        features: &'a [&'a str]
-    ) -> Self {
-        Dependency { name, version, features }
+    const fn new(name: &'a str, version: Option<&'a str>, features: &'a [&'a str]) -> Self {
+        Dependency {
+            name,
+            version,
+            features,
+        }
     }
 }
 
-
-/// 
+///
 /// Dependencies to initialize the ui lib
-/// 
+///
 pub const INIT_DEPENDENCIES: [Dependency<'static>; 2] = [
-    Dependency::new(
-        "leptos", 
-        None, 
-        &["csr"]
-    ),
-    Dependency::new(
-        "tw_merge", 
-        None, 
-        &["variant"]
-    )
-]; 
-
-/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-/*                     ✨ FUNCTIONS ✨                        */
-/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    Dependency::new("leptos", None, &["csr"]),
+    Dependency::new("tw_merge", None, &["variant"]),
+];
--- src/constants/url.rs
+++ src/constants/url.rs
@@ -1,9 +1,8 @@
 pub struct URL;
 
-#[allow(unused)]
 impl URL {
     pub const URL_REGISTRY_INDEX_JSON: &str = "https://www.rust-ui.com/registry/index.json";
-    pub const URL_CONFIG_SCHEMA_JSON: &str = "https://www.rust-ui.com/schema.json";
+    // pub const URL_CONFIG_SCHEMA_JSON: &str = "https://www.rust-ui.com/schema.json";
     pub const URL_REGISTRY_STYLES_JSON: &str = "https://www.rust-ui.com/registry/styles/index.json";
     pub const BASE_URL_STYLES_DEFAULT: &str = "https://www.rust-ui.com/registry/styles/default";
 }
